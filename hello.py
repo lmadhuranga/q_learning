83a71c56a3f921d1c442be498d6d2ebd26a175e8
lmadhuranga <lilan.maduranga@gmail.com>
2020-03-18 00:14:22 +0530

    added basic code segment

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
+
 env = gym.make('MountainCar-v0')
 env.reset()
 
@@ -8,7 +10,7 @@ DISCOUNT = 0.95
 EPISODES = 25000
 
 
-SHOW_EVERY = 2000
+SHOW_EVERY = 10000
 
 DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)
 discrete_os_win_size = (env.observation_space.high - env.observation_space.low) / DISCRETE_OS_SIZE
@@ -28,8 +30,11 @@ def get_discrete_state(state):
   return tuple(discrete_state.astype(np.int))
 
 
-for episode in range(EPISODES): 
+ep_rewards = []
+aggr_ep_rewards = {'ep': [], 'avg':[], 'min':[], 'max':[]}
 
+for episode in range(EPISODES): 
+  episode_reward = 0
   if episode % SHOW_EVERY == 0:
     render = True
   
@@ -47,6 +52,7 @@ for episode in range(EPISODES):
   while not done:
     action = np.argmax(q_table[discrete_state])
     new_state, reward, done, _ = env.step(action)
+    episode_reward += reward
     new_discrete_state = get_discrete_state(new_state)
     
     if render:
@@ -68,4 +74,19 @@ for episode in range(EPISODES):
   if END_EPSILON_DECAYING >= episode >= START_EPSILON_DECAYING:
     epsilon -= epsilon_decaying_value
 
+  ep_rewards.append(episode_reward)
+  if not episode % SHOW_EVERY:
+    average_reward = sum(ep_rewards[-SHOW_EVERY:])/len(ep_rewards[-SHOW_EVERY:])
+    aggr_ep_rewards['ep'].append(episode)
+    aggr_ep_rewards['avg'].append(average_reward)
+    aggr_ep_rewards['min'].append(min(ep_rewards[-SHOW_EVERY:]))
+    aggr_ep_rewards['max'].append(max(ep_rewards[-SHOW_EVERY:])) 
+
+    print(f"Episode: {episode} avg: {average_reward} min: {aggr_ep_rewards['min']} max: {aggr_ep_rewards['max']}")
 env.close()
+
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['avg'], label='avg')
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['min'], label='min')
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['max'], label='max')
+# plt.legend(loca=4)
+plt.show()
